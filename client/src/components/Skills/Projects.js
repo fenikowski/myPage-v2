3a9d6831d61529663cbb52698fd52790436b27b1
Igor Fenikowski <fenikowski@gmail.com>
2023-07-04 11:45:34 +0200

    Remove dialog window from projects

--- client/src/components/Skills/Projects.js
+++ client/src/components/Skills/Projects.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 
-// import ReactScreen from "../../img/ReactScreen.png";
 import ToDoScreen from "../../img/todolistscreen.png";
 import BiuroScreen from "../../img/biuroScreen.png";
 import BiuroScreen2 from "../../img/biuroScreen2.png";
@@ -28,16 +27,13 @@ import JavaSriptLogo from "../../img/javaScriptLogo.png";
 import GimpLogo from "../../img/GimpLogo.png";
 import MySQLLogo from "../../img/mysql.svg";
 
-import Data from "../../text";
-
 import "./styles/projects.css"
 
-export default function Projects({ language }) {
+export default function Projects() {
   // refs
   const projectsSection = useRef(null);
 
   // states
-  const [show, setShow] = useState("all");
   const [hover, setHover] = useState("none");
   const [pictures, setPictures] = useState(Array(9).fill(0));
 
@@ -59,17 +55,6 @@ export default function Projects({ language }) {
     });
   };
 
-  const onProjectClick = display => {
-    setShow(display);
-
-    if (typeof display === "number") {
-      window.scrollTo({
-        top: projectsSection.current.offsetTop,
-        behaviour: "smooth"
-      });
-    }
-  };
-
   const onProjectHover = display => {
     setHover(display);
   };
@@ -80,56 +65,28 @@ export default function Projects({ language }) {
       pictures: [CharityScreen, CharityScreen2, CharityScreen3],
       technologies: [JavaSriptLogo, ReactLogo],
       link: "https://template-charity-tayga.herokuapp.com/en/",
-      date: "September 2019",
-      state: "In development",
-      github: "",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "eng",
-      frameworks: "react",
-      description: `A template of a webiste made for a charity.`
+      date: "September 2019"
     },
     {
       title: "Tayga website",
       pictures: [TaygaScreen, TaygaScreen2],
       technologies: [ReactLogo, JavaSriptLogo, NodeLogo, MySQLLogo],
       link: "http://app.tayga.a2hosted.com/",
-      date: "October 2019",
-      state: "In development",
-      github: "",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "eng",
-      frameworks: "express, react, react router",
-      description: `Website for IT company startup`
+      date: "October 2019"
     },
     {
       title: "Oldschool barber website",
       pictures: [NiceBarber, NiceBarber2],
       technologies: [ReactLogo, JavaSriptLogo],
       link: "https://barber-template2-tayga.herokuapp.com/",
-      date: "October 2019",
-      state: "Finished",
-      github: "",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "en",
-      frameworks: "react, react router",
-      description: `Website template for a hairdresser`
+      date: "October 2019"
     },
     {
       title: "Designer barber website",
       pictures: [DesignerBarber, DesignerBarber2],
       technologies: [ReactLogo, JavaSriptLogo],
       link: "https://taygaa09jdad.herokuapp.com/",
-      date: "October 2019",
-      state: "Finished",
-      github: "",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "es",
-      frameworks: "react, react router",
-      description: `Website template for a hairdresser`
+      date: "October 2019"
     },
     {
       title: "ToDoApp",
@@ -137,13 +94,7 @@ export default function Projects({ language }) {
       technologies: [JavaSriptLogo, NodeLogo, MongoLogo],
       link: "http://www.todoapp.fenikowski.site/",
       date: "July 2019",
-      state: "Finished",
-      github: "https://github.com/fenikowski/todoapp",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "eng",
-      frameworks: "express, mongodb",
-      description: `Simple 'to do' app written in node js with a use of mongo database.`
+      github: "https://github.com/fenikowski/todoapp"
     },
     {
       title: "Accounting Office Site",
@@ -151,15 +102,7 @@ export default function Projects({ language }) {
       technologies: [ReactLogo, NodeLogo, MongoLogo],
       link: "http://www.biurokonfort.pl/",
       date: "August 2019",
-      state: "In progress",
-      github: "https://github.com/fenikowski/AccountOfficeWebsite",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "pl",
-      frameworks:
-        "axios, react, react-cookie, react-dom, react-router-dom, react-scripts, universal-cookie, body-parser, cors, express, mongodb, morgan, nodemailer",
-      description:
-        "Freelance contract project - a website for accounting office. Not finished yet."
+      github: "https://github.com/fenikowski/AccountOfficeWebsite"
     },
     {
       title: "MyPage",
@@ -167,76 +110,28 @@ export default function Projects({ language }) {
       technologies: [ReactLogo, NodeLogo, MongoLogo],
       link: "http://www.fenikowski.site/",
       date: "May 2019",
-      state: "Finished",
-      github: "https://github.com/fenikowski/myPage-v2",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "esp & eng",
-      frameworks:
-        "axios, react, react-cookie, react-dom, react-router-dom, react-scripts, react-simplebar, body-parser, cors, express, mongodb, morgan, nodemailer",
-      description: `My portfolio.`
+      github: "https://github.com/fenikowski/myPage-v2"
     },
     {
       title: "Super Potato Bros",
-      pictures: [
-        SuperPotatoBros,
-        SuperPotatoBros4,
-        SuperPotatoBros2,
-        SuperPotatoBros3
-      ],
+      pictures: [SuperPotatoBros, SuperPotatoBros4, SuperPotatoBros2, SuperPotatoBros3 ],
       technologies: [JavaSriptLogo, GimpLogo],
       link: "http://fenikowski.site/SuperPotatoBros/stage1.html",
       date: "April 2019",
-      state: "Developing",
-      github: "https://github.com/fenikowski/SuperPotatoBros",
-      design: "Igor Fenikowski",
-      code: "Igor Fenikowski",
-      language: "eng",
-      frameworks: "No frameworks used",
-      description:
-        "A game written as a part of my growth as a javascript programmer. An idea of my beloved girlfriend materialised."
-    },
-    // {
-    //   title: "projectName",
-    //   pictures: [ReactScreen],
-    //   technologies: [ReactLogo, NodeLogo],
-    //   link: "test",
-    //   date: "test",
-    //   state: "test",
-    //   github: "test",
-    //   design: "test",
-    //   code: "test",
-    //   language: "test",
-    //   frameworks: "test",
-    //   description: "test"
-    // }
+      github: "https://github.com/fenikowski/SuperPotatoBros"
+    }
   ];
 
-  const addClass = index => {
-    switch (show) {
+  const addClassOnHover = index => {
+    switch (hover) {
       case index:
-        return "show-project";
+        return "hover-project";
 
-      case "all":
+      case "none":
         return "";
 
       default:
-        return "hidden";
-    }
-  };
-
-  const addClassOnHover = index => {
-    if (show === "all") {
-      switch (hover) {
-        case index:
-          return "hover-project";
-
-        case "none":
-          return "";
-
-        default:
-          return "hover-hide";
-      }
+        return "hover-hide";
     }
   };
 
@@ -249,12 +144,10 @@ export default function Projects({ language }) {
     };
     setPictures([...pictures.slice(0,index), newPictureValue,...pictures.slice(index + 1)]);
   };
-  
-  const text = Data[language].projects;
 
   const projectsComponents = projects.map((project, index) => (
     <div
-      className={`project ${addClass(index)} ${addClassOnHover(index)}`}
+      className={`project ${addClassOnHover(index)}`}
       style={{
         backgroundImage: `url(${
           project.pictures[pictures[index]]
@@ -271,59 +164,10 @@ export default function Projects({ language }) {
       >
         <i className="fas fa-chevron-left"></i>
       </button>
-      <button onClick={show === "all" ? () => onProjectClick(index) : () => {}} className="show-more"/>
+      <button onClick={() => {}} className="show-more"/>
       <button onClick={() => slide(index,"right",project.pictures.length)} className="slide right">
         <i className="fas fa-chevron-right"></i>
       </button>
-      <div className="info-to-show">
-        <p>
-          {text.title}
-          {project.title}
-        </p>
-        <p>
-          {text.date}
-          {project.date}
-        </p>
-        <p>
-          {text.state}
-          {project.state}
-        </p>
-        <p>
-          {text.link}
-          <a href={project.link} target="_blank" rel="noreferrer">{project.link}</a>
-        </p>
-        <p>
-          Github:
-          <a href={project.github} target="_blank" rel="noreferrer">{project.github}</a>
-        </p>
-        <p>
-          {text.language}
-          {project.language}
-        </p>
-        <p>
-          {text.code}
-          {project.code}
-        </p>
-        <p>
-          {text.design}
-          {project.design}
-        </p>
-        <p>Frameworks: {project.frameworks}</p>
-        <p>
-          {text.description}
-          {project.description}
-        </p>
-      </div>
-      <button
-        className="close-project"
-        onClick={() => onProjectClick("all")}
-      >
-        <i className="fas fa-times"></i>
-      </button>
-      <div className="project-footer">
-        <p>{project.title}</p>
-        {project.technologies.map((pic, index) => (<img src={pic} key={index} alt="" />))}
-      </div>
     </div>
   ));
 
